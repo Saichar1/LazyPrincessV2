4bc424d66d028c943a846dc462374d19d93429d3
Chowdary <131300937+Chowdary106@users.noreply.github.com>
2023-08-08 00:21:41 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'{SHORT_URL}/api'
+    url = f'{URL_SHORTENR_WEBSITE}/api'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
